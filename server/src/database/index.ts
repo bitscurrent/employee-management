41cc6dc05c9db8c6781e94cfdc782a860b6afe28
dilip <dilipboidya.office@gmail.com>
2024-07-23 14:45:20 +0530

    fix - the USER keyword ceates bug

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -4,12 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER, // The only 'USER' keyword in linux os created problem, so I added DB_USER instead.
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432", 10),
 });
+
+// console.log(process.env.USER, process.env.PASSWORD); 
 
 const connectDB = async (): Promise<void> => {
   pool.on("connect", () => {
